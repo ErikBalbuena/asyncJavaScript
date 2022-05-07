793abba4520fcbd8fa53c497fc5e30f4913af3ce
Erik Balbuena <balbuena@ciencias.unam.mx>
2022-05-06 21:20:59 -0500

    Tercer commit del dìa

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ return callback(a,b);
 
 console.log(calc(1,2,suma));
 
+//para esperar que algo suceda
+
+function fecha(callback){
+console.log(new Date);
+const tiempo = 3000;
+setTimeout(function (){
+    let date = new Date;
+    callback(date);
+},tiempo);
+}
+
+function printDate(dateNow){
+    console.log(dateNow);
+}
+
+fecha(printDate);
